ed0e76a3b30f101d3a1e0afcb9865b844f91ca49
Rafael Gagliardi Maciel da Cruz <rcruz4513@yahoo.com.br>
2023-06-15 15:57:35 -0300

    atualização

---
+++ jogo.js
@@ -0,0 +1,83 @@
+var altura = 0
+var largura = 0
+var vidas = 1
+
+function ajustaTamanhoPalcoJogo() {
+    altura = window.innerHeight
+    largura = window.innerWidth
+
+    console.log(largura, altura)
+}
+
+ajustaTamanhoPalcoJogo()
+
+//Posicão aleatória do mosquito
+function posicaoRandomica() {
+
+    //Remover o mosquito anterior (caso exista)
+    if(document.getElementById('mosquito')){
+        document.getElementById('mosquito').remove()
+
+        if(vidas > 3) {
+            alert('Game over')
+        } else {
+            document.getElementById('v' + vidas).src='imagens/coracao_vazio.png'
+
+            vidas++
+        }
+        
+    }  
+
+    var posicaox = Math.floor(Math.random() * largura) - 90
+    var posicaoy = Math.floor(Math.random() * altura) - 90
+
+    posicaox = posicaox < 0 ? 0 : posicaox
+    posicaoy = posicaoy < 0 ? 0 : posicaoy
+
+    console.log(posicaox, posicaoy)
+
+    //Criar o elemento html
+    var mosquito = document.createElement('img')
+    mosquito.src = 'imagens/mosca.png'
+    mosquito.className = tamanhoAleatorio() + ' ' + ladoAleatorio()
+    mosquito.style.left = posicaox + 'px'
+    mosquito.style.top = posicaoy + 'px'
+    mosquito.style.position = 'absolute'
+    mosquito.id = 'mosquito'
+    mosquito.onclick = function() {
+        this.remove()
+    }
+
+    document.body.appendChild(mosquito)
+
+    console.log(ladoAleatorio())
+
+    
+}
+
+//Tamanho aleatório do mosquito
+function tamanhoAleatorio() {
+    var classe = Math.floor(Math.random() * 3)
+    
+    switch(classe) {
+        case 0:
+            return 'mosquito1'
+        case 1:
+            return 'mosquito2'
+        case 2: 
+            return 'mosquito3'
+    }
+}
+
+//Lado aleatório do mosquito
+function ladoAleatorio() {
+    var classe = Math.floor(Math.random() * 2)
+    
+    switch(classe) {
+        case 0:
+            return 'ladoA'
+        case 1:
+            return 'ladoB'
+        
+    } 
+}
