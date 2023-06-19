d9e1ac599f59f730546631757be55a449b55370c
Rafael Gagliardi Maciel da Cruz <rcruz4513@yahoo.com.br>
2023-06-19 17:01:04 -0300

    finalizado

--- jogo.js
+++ jogo.js
@@ -1,6 +1,23 @@
 var altura = 0
 var largura = 0
 var vidas = 1
+var tempo = 15
+
+var criaMosquitoTempo = 1500
+
+ var nivel = window.location.search
+ nivel = nivel.replace('?', '')
+
+ if(nivel === 'normal') {
+    //1500
+    criaMosquitoTempo = 1500
+ } else if(nivel === 'dificil') {
+    //1000
+    criaMosquitoTempo = 1000
+ } else if(nivel === 'chucknorris') {
+    //750
+    criaMosquitoTempo = 750
+ }
 
 function ajustaTamanhoPalcoJogo() {
     altura = window.innerHeight
@@ -8,6 +25,21 @@ function ajustaTamanhoPalcoJogo() {
 
     console.log(largura, altura)
 }
+
+var cronometro = setInterval(function() {
+
+    tempo -= 1
+
+    if(tempo < 0) {
+        clearInterval(cronometro)
+        clearInterval(criaMosca)
+        window.location.href = 'vitoria.html'        
+    } else {
+        document.getElementById('cronometro').innerHTML = tempo
+    }
+    
+    
+}, 1000)
 
 ajustaTamanhoPalcoJogo()
 
@@ -19,7 +51,7 @@ function posicaoRandomica() {
         document.getElementById('mosquito').remove()
 
         if(vidas > 3) {
-            alert('Game over')
+            window.location.href = 'fim_de_jogo.html'
         } else {
             document.getElementById('v' + vidas).src='imagens/coracao_vazio.png'
 
